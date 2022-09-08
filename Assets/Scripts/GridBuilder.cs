6526b5da2a156d789a698682c91ad5d92edb88f0
Philip Hann <phann@indeed.com>
2022-09-08 15:33:40 -0700

    level gen

---
+++ Assets/Scripts/GridBuilder.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellBuilder
+{
+    public GridBuilder g;
+    public CellBuilder(GridBuilder g)
+    {
+        this.g = g;
+    }
+}
+
+public enum Direction {
+    RIGHT,
+    DOWN,
+    LEFT,
+    UP,
+    RIGHTUP,
+    RIGHTDOWN,
+    LEFTUP,
+    LEFTDOWN,
+}
+
+public class RenameMe {
+    private static readonly Dictionary<Direction, Vector2> _directionToV = new Dictionary<Direction, Vector2>
+    {
+        {Direction.RIGHT, new Vector2(1f, 0f)},
+        {Direction.DOWN, new Vector2(0f, 1f)},
+        {Direction.LEFT, new Vector2(-1f, 0f)},
+        {Direction.UP, new Vector2(0f, -1f)},
+        {Direction.RIGHTUP, new Vector2(1f, -1f)},
+        {Direction.RIGHTDOWN, new Vector2(1f, 1f)},
+        {Direction.LEFTUP, new Vector2(-1f, -1f)},
+        {Direction.LEFTDOWN, new Vector2(-1f, 1f)}
+    };
+
+    public static Dictionary<Direction, Vector2> DirectionToV
+    {
+        get
+        {
+            return _directionToV;
+        }
+    }
+}
+
+public class WallBuilder
+{
+    public GridBuilder g;
+    Vector2 cellLevelCoords;
+    Direction directionFromCell;
+    Vector2 directionFromCellV;
+
+    // bool exists = true;
+    //indicates that this wall is not an important part of the level structure and can
+        // be removed during maze generation
+    // bool removable = true;
+
+    public WallBuilder(GridBuilder g, Vector2 cellLevelCoords, Direction directionFromCell)
+    {
+        this.g = g;
+        this.cellLevelCoords = cellLevelCoords;
+        this.directionFromCell = directionFromCell;
+        this.directionFromCellV = RenameMe.DirectionToV[directionFromCell];
+    }
+
+    public Vector2 getWorldCoords() { 
+        return cellLevelCoords * 2 + directionFromCellV;
+    }
+
+    public Quaternion getRotation() { 
+        return Quaternion.FromToRotation(
+            RenameMe.DirectionToV[Direction.RIGHT],
+            directionFromCellV
+        );;
+    }
+}
+
+public class GridBuilder
+{
+    public CellBuilder[,] cells;
+    public WallBuilder[] walls;
+}
--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -2,89 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CellBuilder
-{
-    public GridBuilder g;
-    public CellBuilder(GridBuilder g)
-    {
-        this.g = g;
-    }
-}
-
-public enum Direction {
-    RIGHT,
-    DOWN,
-    LEFT,
-    UP,
-    RIGHTUP,
-    RIGHTDOWN,
-    LEFTUP,
-    LEFTDOWN,
-}
-
-public class RenameMe {
-    private static readonly Dictionary<Direction, Vector2> _directionToV = new Dictionary<Direction, Vector2>
-    {
-        {Direction.RIGHT, new Vector2(1f, 0f)},
-        {Direction.DOWN, new Vector2(0f, 1f)},
-        {Direction.LEFT, new Vector2(-1f, 0f)},
-        {Direction.UP, new Vector2(0f, -1f)},
-        {Direction.RIGHTUP, new Vector2(1f, -1f)},
-        {Direction.RIGHTDOWN, new Vector2(1f, 1f)},
-        {Direction.LEFTUP, new Vector2(-1f, -1f)},
-        {Direction.LEFTDOWN, new Vector2(-1f, 1f)}
-    };
-
-    public static Dictionary<Direction, Vector2> DirectionToV
-    {
-        get
-        {
-            return _directionToV;
-        }
-    }
-}
-
-public class WallBuilder
-{
-    public GridBuilder g;
-    Vector2 cellLevelCoords;
-    Direction directionFromCell;
-    Vector2 directionFromCellV;
-
-    public WallBuilder(GridBuilder g, Vector2 cellLevelCoords, Direction directionFromCell)
-    {
-        this.g = g;
-        this.cellLevelCoords = cellLevelCoords;
-        this.directionFromCell = directionFromCell;
-        this.directionFromCellV = RenameMe.DirectionToV[directionFromCell];
-    }
-
-    public Vector2 getWorldCoords() { 
-        return cellLevelCoords * 2 + directionFromCellV;
-    }
-
-    public Quaternion getRotation() { 
-        return Quaternion.FromToRotation(
-            RenameMe.DirectionToV[Direction.RIGHT],
-            directionFromCellV
-        );;
-    }
-}
-
-public class GridBuilder
-{
-    public CellBuilder[,] cells;
-    public WallBuilder[] walls;
-}
-
 public class LevelGenerator : MonoBehaviour
 {
     public GameObject wallPrefab;
+    GridBuilder g;
 
-    void Start()
+    void initGrid()
     {
         int width = 10;
-        GridBuilder g = new GridBuilder();
+        this.g = new GridBuilder();
         g.cells = new CellBuilder[width, width];
         int nWalls = 2 * width * width + 2 * width;
         g.walls = new WallBuilder[nWalls];
@@ -111,11 +37,29 @@ public class LevelGenerator : MonoBehaviour
         }
         // Expect exactly nWalls to have been created
         Debug.Assert(wallI == nWalls);
+    }
 
+    void generateLevel() {
+        initGrid();
+
+        
+
+    }
+
+    void instantiateEntities()
+    {
         foreach (WallBuilder w in g.walls) {
+            // if (!w.exists) {
+            //     continue;
+            // }
             Instantiate(wallPrefab, w.getWorldCoords() + new Vector2(5f, 5f), w.getRotation());
         }
+    }
 
+    void Start()
+    {
+        generateLevel();
+        instantiateEntities();
     }
 
     void Update()
