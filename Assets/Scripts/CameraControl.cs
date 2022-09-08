07c02ae897ed362f67b725b27146005c285468b0
Philip Hann <phann@indeed.com>
2022-09-08 14:50:50 -0700

    camera

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3 (player.position.x + offset.x, player.position.y + offset.y, offset.z);
+    }
+}
